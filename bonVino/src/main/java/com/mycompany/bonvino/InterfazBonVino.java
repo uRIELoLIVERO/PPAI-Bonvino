93de64dd7f549a7c88dee1c75d0450d1eb763704
belramello <belenramello@gmail.com>
2024-06-02 01:21:54 -0300

    pantalla principal con menú de despliegue

--- bonVino/src/main/java/com/mycompany/bonvino/InterfazBonVino.java
+++ bonVino/src/main/java/com/mycompany/bonvino/InterfazBonVino.java
@@ -21,7 +21,8 @@ public class InterfazBonVino extends javax.swing.JFrame {
      */
     public InterfazBonVino() {
         initComponents();
-        this.setLocationRelativeTo(null);
+         this.setLocationRelativeTo(null);
+         SetImageLabel (jLabel1, "src/main/resources/fotovino.png");
     }
 
     /**
@@ -35,7 +36,12 @@ public class InterfazBonVino extends javax.swing.JFrame {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel2 = new javax.swing.JPanel();
-        panelRound1 = new com.mycompany.bonvino.PanelRound();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        PanelMenu = new javax.swing.JPanel();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(166, 57, 57));
@@ -44,46 +50,68 @@ public class InterfazBonVino extends javax.swing.JFrame {
         jPanel2.setBackground(new java.awt.Color(166, 57, 57));
         jPanel2.setMaximumSize(new java.awt.Dimension(1080, 321321));
         jPanel2.setMinimumSize(new java.awt.Dimension(1080, 720));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        panelRound1.setBackground(new java.awt.Color(244, 237, 222));
-        panelRound1.setMinimumSize(new java.awt.Dimension(880, 480));
-        panelRound1.setRoundBottomLeft(30);
-        panelRound1.setRoundBottomRight(30);
-        panelRound1.setRoundTopLeft(30);
-        panelRound1.setRoundTopRight(30);
+        jLabel2.setFont(new java.awt.Font("Gilda Display", 1, 56)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Bonvino");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 60, 230, -1));
 
-        javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
-        panelRound1.setLayout(panelRound1Layout);
-        panelRound1Layout.setHorizontalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 880, Short.MAX_VALUE)
-        );
-        panelRound1Layout.setVerticalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
-        );
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/botonMenu.png"))); // NOI18N
+        jButton1.setText("jButton1");
+        jButton1.setBorder(null);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 60, 50, 60));
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(106, Short.MAX_VALUE)
-                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(94, 94, 94))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(120, 120, 120)
-                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(120, Short.MAX_VALUE))
-        );
+        PanelMenu.setBackground(new java.awt.Color(135, 44, 44));
+        PanelMenu.setToolTipText("");
+        PanelMenu.setAlignmentX(0.0F);
+        PanelMenu.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        PanelMenu.setEnabled(false);
+        PanelMenu.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton3.setBackground(new java.awt.Color(135, 44, 44));
+        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("GENERAR RANKING VINO");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        PanelMenu.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 340, 50));
+
+        jButton4.setBackground(new java.awt.Color(135, 44, 44));
+        jButton4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(255, 255, 255));
+        jButton4.setText("IMPORTAR ACTUALIZACIÓN DE VINOS DE BODEGA");
+        PanelMenu.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 50, 340, 60));
+
+        jPanel2.add(PanelMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, 340, 110));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/fotoVinoPrueba.png"))); // NOI18N
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1080, 450));
 
         getContentPane().add(jPanel2, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handing code here:
+        if (PanelMenu.isVisible()){
+            PanelMenu.setVisible(false);
+        }else{
+        PanelMenu.setVisible(true);}
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -121,6 +149,8 @@ public class InterfazBonVino extends javax.swing.JFrame {
     }
     private void SetImageLabel (JLabel labelName, String root){
         ImageIcon image = new ImageIcon (root);
+        labelName.setIcon(null);
+    
         Icon icon = new ImageIcon(
             image.getImage().getScaledInstance(labelName.getWidth(), labelName.getHeight(), Image.SCALE_DEFAULT));
         labelName.setIcon(icon);
@@ -128,8 +158,13 @@ public class InterfazBonVino extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PanelMenu;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel2;
-    private com.mycompany.bonvino.PanelRound panelRound1;
     // End of variables declaration//GEN-END:variables
 }
